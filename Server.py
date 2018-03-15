4b842f6a444c261498cd87c3a50f357863e28537
gf <gongfengzztx@163.com>
2018-03-15 23:25:19 +0800

    socket+threading fail
    function:socket服务端引入threading多线程处理
    run:fail.
    author:will
    note:先上传,明天修改

--- Server.py
+++ Server.py
@@ -1,15 +1,18 @@
 import socket
 import time
+import threading
 
 
 HOST = '127.0.0.1'
 PORT = 50007
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    sock.bind((HOST, PORT))
-    sock.listen(1)
+THREAD_NUM = 100
+
+
+def thread_fun(sock):
     conn, addr = sock.accept()
     with conn:
-        print('connected by', addr)
+        print('conn', conn)
+        print('connected by', addr[0], addr[1])
         while True:
             print(time.ctime())
             data = conn.recv(1024)
@@ -17,3 +20,28 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
             if data == b'0':
                 break
             conn.sendall(data.upper())
+
+
+class SocketThread(threading.Thread):
+    def __init__(self, func, args, name='new thread'):
+        threading.Thread.__init__(self)
+        self.func = func
+        self.args = args
+        self.name = name
+
+    def run(self):
+        self.func(*self.args)
+
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    sock.bind((HOST, PORT))
+    sock.listen(THREAD_NUM)
+    threads = []
+    for i in range(10):
+        threads.append(SocketThread(thread_fun, sock))
+
+    for i in range(10):
+        threads[i].start()
+
+    for i in range(10):
+        threads[i].join()
