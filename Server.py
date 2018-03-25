f2f6c6b0089474092505ef0535338e7258fded9f
gf <gongfengzztx@163.com>
2018-03-25 22:45:26 +0800

    支持多客户端
    function:支持多客户端
    run:未测试
    author:gf
    note:

--- Server.py
+++ Server.py
@@ -1,47 +1,91 @@
 import socket
 import time
 import threading
+import json
+import collections
 
 
 HOST = '127.0.0.1'
 PORT = 50007
 THREAD_NUM = 100
+BUF_SIZE = 1024
 
 
-def thread_fun(sock):
-    conn, addr = sock.accept()
-    with conn:
-        print('conn', conn)
-        print('connected by', addr[0], addr[1])
-        while True:
-            print(time.ctime())
-            data = conn.recv(1024)
-            print(data)
-            if data == b'0':
-                break
-            conn.sendall(data.upper())
-
-
-class SocketThread(threading.Thread):
-    def __init__(self, func, args, name='new thread'):
+class ClientSocket(threading.Thread):
+    """
+    一个客户端一个ClientSocket，负责收发数据
+    """
+    def __init__(self, sock, address, name='client'):
         threading.Thread.__init__(self)
-        self.func = func
-        self.args = args
-        self.name = name
+        self._sock = sock
+        self._address = address
+        self._name = name
+        self._msg_deque = collections.deque(maxlen=10)
+        self.send_message = SendThread(self.sock, self._address, self._msg_deque)
+        self.send_message.start()
 
     def run(self):
-        self.func(*self.args)
+        while True:
+            msg = self._sock.recv(BUF_SIZE)
+            self._msg_deque.append(ClientMessage(msg))
 
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    sock.bind((HOST, PORT))
-    sock.listen(THREAD_NUM)
+class SendThread(threading.Thread):
+    """
+    一个ClientSocket用来发送数据
+    """
+    def __init__(self, sock, address, msg, name='send_thread'):
+        self._sock = sock
+        self._address = address
+        self._name = name
+        self._msg = msg
+
+    def run(self):
+        while True:
+            try:
+                send_msg = self._msg.popleft().msg
+            except IndexError:
+                continue
+            print("send message")
+            self._sock.sendall(send_msg)
+
+
+class ClientMessage:
+    """
+    序列化和反序列化接收到的数据
+    """
+    def __init__(self, msg):
+        self._msg = msg
+
+    @property
+    def msg(self):
+        return self._serialize()
+
+    def _deserialize(self):
+        try:
+            self._msg = json.loads(self._msg.decode())
+            try:
+                if self._msg['type'] == 'login':
+                    if self._msg['content'] == '123':
+                        self._msg['content'] = 'true'
+            except KeyError:
+                print('msg has not type key')
+        except json.JSONDecodeError:
+            print('json decoder fail')
+
+    def _serialize(self):
+        self.deserialize()
+        return json.dumps(self._msg.encode())
+
+
+if __name__ == '__main__':
     threads = []
-    for i in range(10):
-        threads.append(SocketThread(thread_fun, sock))
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.bind((HOST, PORT))
+        sock.listen(THREAD_NUM)
+        while True:
+            print('wait accept client...')
+            conn, address = sock.accept()
+            send_thread = ClientSocket(conn, address)
+            threads.append(send_thread)
 
-    for i in range(10):
-        threads[i].start()
-
-    for i in range(10):
-        threads[i].join()
